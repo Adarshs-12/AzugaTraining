12fc21574db6e02ac9ebb0b732051355bad6da1e
Adarshs-12 <adarshkumarsahu.1996@gmail.com>
2022-09-20 20:46:01 +0530

    converts csv to pdf

--- CSVtoPDF.java
+++ CSVtoPDF.java
@@ -1,42 +1,39 @@
-/*
- * Copyright (c) 2022.  - All Rights Reserved
- *  * Unauthorized copying or redistribution of this file in source and binary forms via any medium
- *  * is strictly prohibited-
- *  * @Author - Adarsh.
- */
-
 package com.training.day5;
-import java.io.FileOutputStream;
-import java.io.*;
+
 import au.com.bytecode.opencsv.CSVReader;
-import java.io.FileReader;
 import com.itextpdf.text.*;
-import com.itextpdf.text.pdf.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * This class converts the csv file to pdf file
+ */
 public class CSVtoPDF {
     public static void main(String[] args) throws IOException, DocumentException {
-
-        String CSVFile = "/Users/azuga/Desktop/weather1.csv";        //Read input CSV file in Java
-        CSVReader reader = new CSVReader(new FileReader(CSVFile));
-        /* Variables to loop through the CSV File */
-        String [] nextLine; /* for every line in the file */
-        int lineNumber = 0; /* line number */
-        /* Step-2: Initialize PDF documents - logical objects */
-        Document my_pdf_data = new Document();
-        PdfWriter.getInstance(my_pdf_data, new FileOutputStream("/Users/azuga/Desktop/converted_PDF_File.pdf"));
+        CSVReader reader = new CSVReader(new FileReader("/Users/azuga/Desktop/weather1.csv"));//reading csv file from given path
+        String[] nextLine;
+        Rectangle rc=new Rectangle(7000f,6500f);
+        Document my_pdf_data = new Document(rc);
+        PdfWriter.getInstance(my_pdf_data, new FileOutputStream("/Users/azuga/Desktop/converted_PDF_File.pdf"));//writing pdf file to given path
         my_pdf_data.open();
-        PdfPTable my_first_table = new PdfPTable(15);
+        PdfPTable my_first_table = new PdfPTable(42);
+
+
         PdfPCell table_cell;
-        /* Step -3: Loop through CSV file and populate data to PDF table */
         while ((nextLine = reader.readNext()) != null) {
-            lineNumber++;
-            int i=0;
-            while(i<=42) {
+            int i = 0;
+            while (i < 42) {
                 table_cell = new PdfPCell(new Phrase(nextLine[i]));
                 my_first_table.addCell(table_cell);
                 i++;
             }
         }
-        /* Step -4: Attach table to PDF and close the document */
+
+
         my_pdf_data.add(my_first_table);
         my_pdf_data.close();
     }
