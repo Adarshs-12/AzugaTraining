fd1f3a8d43f636880262d80226f0bb0a47afc26d
Adarshs-12 <adarshkumarsahu.1996@gmail.com>
2022-10-07 13:28:50 +0530

    Java Program to UnZip files

---
+++ Email Zip UnZip/UnzipClass.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2022.  - All Rights Reserved
+ * Unauthorized copying or redistribution of this file in source and binary forms via any medium
+ * is strictly prohibited-
+ * @Author -Adarsh (adarshs@azuga.com).
+ */
+package com.training.week4;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * This class converts the csv file to pdf file
+ */
+public class UnzipClass {
+
+    public static void main(String[] args) {
+
+        Path source = Paths.get("/Users/azuga/Desktop/zipfile.zip");
+        Path target = Paths.get("/Users/azuga/Desktop/");
+
+        try {
+
+            unzipFolder(source, target);
+            System.out.println("Done");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void unzipFolder(Path source, Path target) throws IOException {
+
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(source.toFile()))) {
+
+            // list files in zip
+            ZipEntry zipEntry = zis.getNextEntry();
+
+            while (zipEntry != null) {
+
+                boolean isDirectory = zipEntry.getName().endsWith(File.separator);
+
+                Path newPath = zipSlipProtect(zipEntry, target);
+
+                if (isDirectory) {
+                    Files.createDirectories(newPath);
+                } else {
+
+                    // example 1.2
+                    // some zip stored file path only, need create parent directories
+                    // e.g data/folder/file.txt
+                    if (newPath.getParent() != null) {
+                        if (Files.notExists(newPath.getParent())) {
+                            Files.createDirectories(newPath.getParent());
+                        }
+                    }
+
+                    // copy files, nio
+                    Files.copy(zis, newPath, StandardCopyOption.REPLACE_EXISTING);
+
+                }
+
+                zipEntry = zis.getNextEntry();
+
+            }
+            zis.closeEntry();
+
+        }
+
+    }
+
+    // protect zip slip attack
+    public static Path zipSlipProtect(ZipEntry zipEntry, Path targetDir)
+            throws IOException {
+
+        // test zip slip vulnerability
+        // Path targetDirResolved = targetDir.resolve("../../" + zipEntry.getName());
+
+        Path targetDirResolved = targetDir.resolve(zipEntry.getName());
+
+        // make sure normalized file still has targetDir as its prefix
+        // else throws exception
+        Path normalizePath = targetDirResolved.normalize();
+        if (!normalizePath.startsWith(targetDir)) {
+            throw new IOException("Bad zip entry: " + zipEntry.getName());
+        }
+
+        return normalizePath;
+    }
+
+}
