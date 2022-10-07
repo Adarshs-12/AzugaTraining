8af52328bbd60e227e2798a2024753ba584e2aa7
Adarshs-12 <adarshkumarsahu.1996@gmail.com>
2022-10-07 13:29:24 +0530

    Java Program to Zip Files

---
+++ Email Zip UnZip/ZipClass.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2022.  - All Rights Reserved
+ * Unauthorized copying or redistribution of this file in source and binary forms via any medium
+ * is strictly prohibited-
+ * @Author -Adarsh (adarshs@azuga.com).
+ */
+package com.training.week4;
+
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+
+/**
+ * This class converts the folder into Zip file
+ */
+public class ZipClass {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream("/Users/azuga/Desktop/zipfile.zip");
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+        File fileToZip = new File("/Users/azuga/Desktop/zipfile");
+
+        zipFile(fileToZip, fileToZip.getName(), zipOut);
+        zipOut.close();
+        fos.close();
+    }
+
+    private static void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
+        if (fileToZip.isHidden()) {
+            return;
+        }
+        if (fileToZip.isDirectory()) {
+            if (fileName.endsWith("/")) {
+                zipOut.putNextEntry(new ZipEntry(fileName));
+                zipOut.closeEntry();
+            } else {
+                zipOut.putNextEntry(new ZipEntry(fileName + "/"));
+                zipOut.closeEntry();
+            }
+            File[] children = fileToZip.listFiles();
+            assert children != null;
+            for (File childFile : children) {
+                zipFile(childFile, fileName + "/" + childFile.getName(), zipOut);
+            }
+            return;
+        }
+        FileInputStream fis = new FileInputStream(fileToZip);
+        ZipEntry zipEntry = new ZipEntry(fileName);
+        zipOut.putNextEntry(zipEntry);
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
+        }
+        fis.close();
+    }
+}
